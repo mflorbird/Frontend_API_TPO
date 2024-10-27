9a5dbe0dc641f95cd2a83d72b2f45e08b9ce91e1
Natesta13 <natesta@uade.edu.ar>
2024-10-27 14:56:37 -0300

    agregue una pagina para el carrito vacio, se agrega tambien a las rutas. pueden probarla con /cartEmpty

--- Frontend_API_TPO/src/routes/AppRoutes.jsx
+++ Frontend_API_TPO/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home.jsx'; 
 import Cart from '../pages/Cart.jsx'; 
+import CartEmpty from '../pages/CartEmpty.jsx';
 import Navbar from '../components/Navbar.jsx'; 
 import Footer from '../components/Footer.jsx'; 
 
@@ -13,6 +14,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/cartEmpty" element={<CartEmpty />} /> {/*agrego una mas*/}
         {/* agregar mas rutas*/}
       </Routes>
       </div>
