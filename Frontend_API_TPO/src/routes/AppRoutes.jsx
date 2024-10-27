53927986bcde3e33cb17aa4fee5ffe8dc7c738a9
florbird <mabird@uade.edu.ar>
2024-10-27 13:56:20 -0300

    arreglo conflictos + agregos login y registro

--- Frontend_API_TPO/src/routes/AppRoutes.jsx
+++ Frontend_API_TPO/src/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home.jsx'; 
 import Cart from '../pages/Cart.jsx'; 
+import Login from '../pages/LoginPage.jsx'; 
+import Register from '../pages/Registro.jsx'; 
 import Navbar from '../components/Navbar.jsx'; 
 import Footer from '../components/Footer.jsx'; 
 
@@ -13,6 +15,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         {/* agregar mas rutas*/}
       </Routes>
       </div>
