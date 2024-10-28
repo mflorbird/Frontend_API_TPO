2099b13d53ee9b871142c0349bc5a05b01711c72
Diego B <dbari@seidoranalytics.com>
2024-10-28 17:33:36 -0300

    Update ProductCard.jsx

--- Frontend_API_TPO/src/components/catalog/ProductCard.jsx
+++ Frontend_API_TPO/src/components/catalog/ProductCard.jsx
@@ -1,26 +1,19 @@
-import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+
 
 const ProductCard = ( { product } ) => {
     return (
-        <div className="card shadow-sm">
-            <img src={product.image} alt={product.name} className="card-img-top" />
-            <div className="card-body">
-                <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">{product.description}</p>
-                <p className="card-text">Price: {product.price}</p>
-            </div>
-        </div>
+        <Card className="col-md-3">
+            <Card.Img variant="top" src={product.image} />
+            <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>
+                    {product.description}
+                    {product.price}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
-
-ProductCard.propTypes = {
-    product: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        image: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-    }).isRequired,
-};
 
 export default ProductCard;
