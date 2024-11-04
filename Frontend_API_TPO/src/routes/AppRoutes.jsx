7e6c85c36b3db163ac49bf0d8aeb55fcd76dc3fd
AlexOde91 <demayo.alex@gmail.com>
2024-11-04 00:01:46 -0300

    Agrego pagina miPerfil

--- Frontend_API_TPO/src/routes/AppRoutes.jsx
+++ Frontend_API_TPO/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home.jsx'; 
 import Cart from '../pages/Cart.jsx'; 
+import Profile from './pages/Profile';
 import Navbar from '../components/Navbar.jsx'; 
 import Footer from '../components/Footer.jsx'; 
 
