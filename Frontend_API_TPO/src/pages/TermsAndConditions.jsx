e2f15daff5180619eb97696c82c0f424601062ea
florbird <mabird@uade.edu.ar>
2024-11-09 11:03:47 -0300

    TermsAndConditions_V1.0
    Agrego términos y condiciones

---
+++ Frontend_API_TPO/src/pages/TermsAndConditions.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import '../styles/TermsAndConditions.css';
+
+const TermsAndConditions = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="terms-and-conditions container my-5">
+        <h1 className="text-center">Términos y Condiciones de Uso</h1>
+        <p className="text-uppercase text-center font-weight-bold mt-4">
+          POR FAVOR LEA ESTE DOCUMENTO CUIDADOSAMENTE Y NO OPERE EN LA PLATAFORMA NAIKII (la "plataforma") SI NO ESTÁ DE ACUERDO CON SUS TÉRMINOS Y CONDICIONES DE USO ("términos").
+        </p>
+        
+        <section className="mt-5">
+          <h2>¡Bienvenido a la comunidad Naikii!</h2>
+          <p>
+            Estos Términos describen las reglas aplicables al uso de un sitio web, experiencia digital, una plataforma de medios sociales, una aplicación móvil, una tecnología portátil de propiedad de Naikii o uno de nuestros otros productos o servicios, todos los cuales forman parte de la plataforma.
+          </p>
+          <p>
+            Usted puede acceder a la plataforma a través de una computadora, teléfono móvil, tableta, consola u otra tecnología similar ("dispositivo"). Las tarifas y gastos de su empresa de telecomunicaciones se aplican a su dispositivo y están a su cargo.
+          </p>
+          <p>
+            Estos Términos constituyen un acuerdo legalmente vinculante entre usted ("usted" o "usuario") y Naikii S.A., en adelante también indistinta y conjuntamente llamados "Naikii", "nosotros", "nos" o "nuestro", con respecto al uso que usted hace de la plataforma.
+          </p>
+          <p>
+            La entidad responsable de proporcionarle la plataforma es Naikii S.A., CUIT 30-12345678-9, con domicilio en Av. Principal 1234, Ciudad de Buenos Aires. Para contactarnos usted dispone de nuestros canales de comunicación, que detallamos aquí. Al hacer clic en el casillero de aceptación, usted declara haber leído detenidamente, comprendido y consentido estos términos.
+          </p>
+        </section>
+
+        <section className="important-points mt-5">
+          <h3>A continuación, listamos algunos puntos importantes para usted:</h3>
+          
+          <h6 className="mt-3 font-weight-bold">- Nuestros términos pueden cambiar</h6>
+          <p className="mb-3">
+            Podemos actualizar estos términos cuando lo consideremos oportuno. Si se realiza un cambio, publicaremos un aviso en la plataforma. Usted deberá leer detenidamente cualquier cambio y se le otorgará la posibilidad de aceptarlo expresamente o no, en los casos en que dicho cambio sea sustancial. Si no está de acuerdo con el cambio, usted tiene la posibilidad de dejar de usar la plataforma.
+          </p>
+
+          <h6 className="mt-3 font-weight-bold">- Términos de venta</h6>
+          <p className="mb-3">
+            Previo a realizar cualquier compra con nosotros, usted también debe aceptar los términos de venta aplicables a su país o región.
+          </p>
+
+          <h6 className="mt-3 font-weight-bold">- Política de privacidad</h6>
+          <p className="mb-3">
+            Nuestra política de privacidad describe la recopilación y el uso de información personal en la plataforma y se aplica a su uso de la plataforma. El usuario consiente en forma expresa, informada, voluntaria e inequívoca el tratamiento de sus datos personales por parte de Naikii, de conformidad con dicha política de privacidad.
+          </p>
+
+          <hr className="my-4" /> {/* Divider */}
+
+          <h4 className="mt-3">1- REGLAS BÁSICAS</h4>
+          <p className="mb-3">
+            Requisitos. Para poder usar la plataforma, usted deberá haber cumplido dieciocho (18) años, no estar declarado judicialmente como incapaz, o tener la capacidad restringida para contratar o disponer de su patrimonio o contar con el consentimiento de sus progenitores, tutor o representante legal. Naikii podrá pedirle documentación que acredite estos extremos para la protección de la comunidad de usuarios.
+          </p>
+
+          <hr className="my-4" /> {/* Divider */}
+
+          <h4 className="mt-3">2- PROPIEDAD DEL CONTENIDO</h4>
+          <p className="mb-3">
+            Excepto por el contenido de usuario (definido a continuación), todo el contenido de nuestra plataforma, incluidos textos, software, gráficos, fotografías, sonidos, videos, aplicaciones, características interactivas, y otros contenidos protegidos por derechos de propiedad intelectual ("contenido"), es propiedad de Naikii S.A. y/o de terceros de los que tenemos licencia, y está protegido por las leyes sobre derechos de propiedad intelectual. Nada de lo dispuesto en estos términos otorga una licencia o cesión de derechos de propiedad intelectual a favor del usuario.
+          </p>
+
+          <hr className="my-4" /> {/* Divider */}
+
+          <h4 className="mt-3">3- CÓDIGO DE CONDUCTA DEL USUARIO</h4>
+          <p className="mb-3">
+            Nos emociona su contribución a la comunidad Naikii. Estas son algunas reglas básicas:
+            <br />
+            - Actúe con responsabilidad. No haga nada que pueda exponer a Naikii o a sus usuarios a cualquier tipo de daño, incluyendo cualquier acción que pueda interrumpir, dañar, deshabilitar, alterar, sobrecargar o limitar la funcionalidad de la plataforma.
+            <br />
+            - Sea usted mismo. No se haga pasar por ninguna persona u organización, incluidos los atletas o empleados de Naikii.
+          </p>
+
+          <hr className="my-4" /> {/* Divider */}
+
+          <h4 className="mt-3">4- TERCEROS EN LA PLATAFORMA</h4>
+          <p className="mb-3">
+            Cuando lo considere oportuno, Naikii puede vincularse o asociarse con sitios web de terceros, plataformas de redes sociales, aplicaciones móviles y otros productos y servicios ("terceros"). Puede conectarse con estos terceros a través de la plataforma, pero esto no significa que Naikii avale, monitoree o tenga control sobre estos terceros o sus actividades, que están sujetos a términos de uso y políticas de privacidad independientes. Naikii no es responsable del contenido, las políticas o las actividades de terceros, y usted decide interactuar con terceros bajo su propio riesgo.
+          </p>
+
+          <hr className="my-4" /> {/* Divider */}
+
+          <h4 className="mt-3">5- RESCISIÓN</h4>
+          <p className="mb-3">
+            De acuerdo a la legislación aplicable, nosotros podremos rescindir o modificar la plataforma, los productos o servicios en cualquier momento, sin previo aviso, por iniciativa propia o por petición de terceros. También podremos restringir el uso que usted hace de la plataforma cuando identifiquemos un uso indebido de la misma, sin responsabilidad imputable a Naikii.
+          </p>
+
+          <hr className="my-4" /> {/* Divider */}
+
+          <h4 className="mt-3">6- INDEMNIZACIÓN</h4>
+          <p className="mb-3">
+            Indemnización y liberación. Usted acepta indemnizar, defender y eximir de responsabilidad a Naikii y sus directivos, empleados, agentes, licenciantes y proveedores de y contra todas las reclamaciones, pérdidas, responsabilidades, gastos y costos que de alguna manera surjan de o en relación con su uso del contenido, su uso de la plataforma o cualquier violación de estos términos, de cualquier ley o de los derechos de terceros.
+          </p>
+
+          <hr className="my-4" /> {/* Divider */}
+
+          <h4 className="mt-3">7- DISPUTAS / TÉRMINOS ADICIONALES</h4>
+          <p className="mb-3">
+            Derecho aplicable / Jurisdicción. La relación entre usted y Naikii se regirá por las leyes de la República Argentina y cualquier controversia se someterá a los Juzgados y/o Tribunales de la jurisdicción correspondiente en razón de las leyes argentinas.
+          </p>
+
+          <hr className="my-4" /> {/* Divider */}
+
+          <h4 className="mt-3">8- ATENCIÓN AL CONSUMIDOR/USUARIO</h4>
+          <p className="mb-3">
+            Al utilizar nuestra plataforma, usted podrá utilizar el chat de Whatsapp que le proporcionaremos, para contactarse con nosotros y manifestar sus consultas, entendiéndose el mismo como un canal de atención al cliente.
+          </p>
+        </section>
+
+        <p className="text-center mt-5 font-weight-bold">Gracias por leer. ¡Disfrute de nuestra comunidad Naikii!</p>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default TermsAndConditions;
