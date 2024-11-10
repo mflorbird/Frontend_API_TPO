c8ad68072b8763a192f1fc1feb3bb5e75a817dc4
Diego B <dbari@seidoranalytics.com>
2024-11-10 04:28:23 -0300

    Update FavoritosList.jsx

--- Frontend_API_TPO/src/components/catalog/FavoritosList.jsx
+++ Frontend_API_TPO/src/components/catalog/FavoritosList.jsx
@@ -1,35 +1,46 @@
 // ProductList.jsx
 import ProductCard from "./ProductCard";
 import { useEffect, useState } from "react";
-import {getDestacados} from "../../services/catalogService.js";
+import { getFavoritos } from "../../services/catalogService.js";
+import { AppContext } from '../../context/AppContext';
+import { useContext } from 'react';
+
 
 const FavoritosList = () => {
-    // const [data, setData] = useState([]);
+
+    const { user } = useContext(AppContext);
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
 
 
-    const data = [
-        { id: 1, name: 'Producto 1', price: 100, image: '01ZAPATILLAS.png', description: 'Producto 1 description' },
-        { id: 2, name: 'Producto 2', price: 200, image: '08ZAPATILLAS.png', description: 'Producto 2 description' },
-        { id: 3, name: 'Producto 3', price: 300, image: '05ZAPATILLAS.png', description: 'Producto 3 description' },
-        { id: 4, name: 'Producto 4', price: 400, image: '01ZAPATILLAS.png', description: 'Producto 4 description' }
-    ];
+    // const data = [
+    //     { id: 1, name: 'Producto 1', price: 100, image: '01ZAPATILLAS.png', description: 'Producto 1 description' },
+    //     { id: 2, name: 'Producto 2', price: 200, image: '08ZAPATILLAS.png', description: 'Producto 2 description' },
+    //     { id: 3, name: 'Producto 3', price: 300, image: '05ZAPATILLAS.png', description: 'Producto 3 description' },
+    //     { id: 4, name: 'Producto 4', price: 400, image: '01ZAPATILLAS.png', description: 'Producto 4 description' }
+    // ];
 
-    // useEffect(() => {
-    //     const loadProducts = async () => {
-    //         try {
-    //             const products = await getFavoritos();
-    //             setData(products);
-    //             setLoading(false);
-    //         } catch (error) {
-    //             console.error(`Error loading products: ${error}`);
-    //             setLoading(false);
-    //         }
-    //     };
-    //     loadProducts();
-    // }, []);
+    useEffect(() => {
+        const loadProducts = async () => {
+            if (user.favoritos.length === 0) {
+                setLoading(false);
+                return;
+            }
+            try {
+                const products = await getFavoritos( user );
+                setData(products);
+                setLoading(false);
+            } catch (error) {
+                console.error(`Error loading products: ${error}`);
+                setLoading(false);
+            }
+        };
+        loadProducts();
+    }, []);
 
-    // if (loading) return <p>Loading...</p>;
+    if (loading) return <p>Loading...</p>;
+
+    if (!data.length) return <p>No hay productos en favoritos</p>;
 
     return (
         <div className="container mt-3">
