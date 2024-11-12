5a2c1180090876957d3b8a8f3e2da0737171b392
Diego B <dbari@seidoranalytics.com>
2024-11-11 23:49:52 -0300

    Update Cart.jsx

--- Frontend_API_TPO/src/pages/Cart.jsx
+++ Frontend_API_TPO/src/pages/Cart.jsx
@@ -7,124 +7,119 @@ import { Button } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 
 const Cart = () => {
-  const navigate = useNavigate();
-  const { user, cart, setDiscount, clearCart, updateCartItemQuantity, removeItemFromCart } = useContext(AppContext);
-  const [discountCode, setDiscountCode] = useState('');
+    const navigate = useNavigate();
+    const { user, cart, setDiscount, clearCart, updateCartItemQuantity, removeItemFromCart } = useContext(AppContext);
+    const [discountCode, setDiscountCode] = useState('');
 
-  // Redirecciona si user o cart son nulos o indefinidos
-  useEffect(() => {
-    if (!user || !cart) {
-      navigate('/error');
-    }
-  }, [user, cart, navigate]);
+    // Redirecciona si user o cart son nulos o indefinidos
+    useEffect(() => {
+        if (!user || !cart) {
+            navigate('/error');
+        }
+    }, [user, cart, navigate]);
 
-  const applyDiscount = () => {
-    if (discountCode === 'NAIKI10') {
-      setDiscount(0.1); // 10% de descuento
-    } else {
-      alert("Código de descuento inválido");
-    }
-  };
+    const applyDiscount = () => {
+        if (discountCode === 'NAIKI10') {
+            setDiscount(0.1); // 10% de descuento
+        } else {
+            alert("Código de descuento inválido");
+        }
+    };
 
-  const handleQuantityChange = (itemId, newQuantity) => {
-    updateCartItemQuantity(itemId, newQuantity);
-  };
+    const handleQuantityChange = (itemId, newQuantity) => {
+        updateCartItemQuantity(itemId, newQuantity);
+    };
 
-  const handleDeleteItem = (itemId) => {
-    removeItemFromCart(itemId);
-  };
+    const handleDeleteItem = (itemId) => {
+        removeItemFromCart(itemId);
+    };
 
-  const cartItems = Object.entries(cart?.items || {});
+    const cartItems = Object.entries(cart?.items || {});
 
-  return (
-      <div className="cart-container">
-        <div className="cart-container-body">
-          <h1 className='mb-4'>Hola {user?.nombre}</h1>
-          <ul className="progress-steps">
-            <li className="step current">Paso 1: Completa tu carrito</li>
-            <li className="step pending">Paso 2: Datos de Envío</li>
-            <li className="step pending">Paso 3: Detalle de Facturación</li>
-            <li className="step pending">Paso 4: Realizar Pago</li>
-          </ul>
-          <p className='mt-4 mb-4'>Visualizá los productos que tienes en tu carrito</p>
-          <div className="cart-content">
-            <div className="cart-items">
-              {cartItems.length === 0 ? (
-                  <p>No hay productos en el carrito.</p>
-              ) : (
-                  cartItems.map(([key, item]) => (
-                      <div key={key} className="cart-item">
-                        <img src={item.img} alt={item.model} width="50" height="50" />
-                        <div className="item-details">
-                          <p><strong>{item.model}</strong></p>
-                          <p>Talle: {item.size || 'N/A'}</p>
-                          <p>Precio: ${item.price}</p>
-                          <p>Total: ${item.price * item.quantity}</p>
+    return (
+        <div className="cart-container">
+            <div className="cart-container-body">
+                <h1>Hola {user?.nombre}</h1>
+                <ul className="progress-steps">
+                    <li className="step current">Paso 1: Completa tu carrito</li>
+                    <li className="step pending">Paso 2: Datos de Envío</li>
+                    <li className="step pending">Paso 3: Detalle de Facturación</li>
+                    <li className="step pending">Paso 4: Realizar Pago</li>
+                </ul>
+                <p>Visualizá los productos que tienes en tu carrito</p>
+                <div className="cart-content">
+                    <div className="cart-items">
+                        {cartItems.length === 0 ? (
+                            <p>No hay productos en el carrito.</p>
+                        ) : (
+                            cartItems.map(([key, item]) => (
+                                <div key={key} className="cart-item">
+                                    <img src={item.img} alt={item.model} width="50" height="50" />
+                                    <div className="item-details">
+                                        <p><strong>{item.model}</strong></p>
+                                        <p>Talle: {item.size || 'N/A'}</p>
+                                        <p>Precio: ${item.price}</p>
+                                        <p>Total: ${item.price * item.quantity}</p>
+                                    </div>
+                                    <div className="quantity mb-3">
+                                        <h5>Cantidad</h5>
+                                        <div className="d-flex align-items-center">
+                                            <Button
+                                                variant="outline-secondary"
+                                                onClick={() => handleQuantityChange(key, Math.max(1, item.quantity - 1))}
+                                            >
+                                                -
+                                            </Button>
+                                            <span className="mx-3">{item.quantity}</span>
+                                            <Button
+                                                variant="outline-secondary"
+                                                onClick={() => handleQuantityChange(key, item.quantity + 1)}
+                                            >
+                                                +
+                                            </Button>
+                                        </div>
+                                    </div>
+                                    <Button
+                                        variant="outline-danger"
+                                        onClick={() => handleDeleteItem(key)}
+                                        style={{ marginLeft: '15px', marginTop: '15px' }}
+                                    >
+                                        <FaTrash />
+                                    </Button>
+                                </div>
+                            ))
+                        )}
+                    </div>
+                    <div className="cart-summary">
+                        <OrderSummary subtotal={cart.precioTotal} discountAmount={cart.discount} totalAmount={cart.precioDiscount} />
+                        <div className="summary-item">
+                            <input
+                                type="text"
+                                placeholder="Código de descuento"
+                                value={discountCode}
+                                onChange={(e) => setDiscountCode(e.target.value)}
+                            />
+                            <button onClick={applyDiscount}>Aplicar Descuento</button>
                         </div>
-                        <div className="quantity mb-3">
-                          <h5>Cantidad</h5>
-                          <div className="d-flex align-items-center">
-                            <Button
-                                variant="outline-secondary"
-                                onClick={() => handleQuantityChange(key, Math.max(1, item.quantity - 1))}
-                            >
-                              -
-                            </Button>
-                            <span className="mx-3">{item.quantity}</span>
-                            <Button
-                                variant="outline-secondary"
-                                onClick={() => handleQuantityChange(key, item.quantity + 1)}
-                            >
-                              +
-                            </Button>
-                          </div>
-                        </div>
-                        <Button
-                            variant="outline-danger"
-                            onClick={() => handleDeleteItem(key)}
+                        <button
+                            onClick={() => navigate("/cartEnvio")}
                             style={{ marginLeft: '15px', marginTop: '15px' }}
+                            disabled={cartItems.length === 0}
                         >
-                          <FaTrash />
-                        </Button>
-                      </div>
-                  ))
-              )}
+                            Siguiente
+                        </button>
+                        <button
+                            onClick={clearCart}
+                            className="clear-cart-button"
+                            disabled={cartItems.length === 0}
+                        >
+                            Vaciar Carrito
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="cart-summary">
-              <OrderSummary subtotal={cart.precioTotal} discountAmount={cart.discount} totalAmount={cart.precioDiscount} />
-              <div className="summary-item">
-                <input
-                    type="text"
-                    placeholder="Código de descuento"
-                    value={discountCode}
-                    onChange={(e) => setDiscountCode(e.target.value)}
-                />
-                <button onClick={applyDiscount}>Aplicar Descuento</button>
-              </div>
-<<<<<<< Updated upstream
-              <button className="mt-4" onClick={() => navigate("/cartEnvio")}>Siguiente</button>
-=======
-              {/* Botones "Siguiente" y "Vaciar Carrito" deshabilitados si no hay items en el carrito */}
-              <button
-                  onClick={() => navigate("/cartEnvio")}
-                  disabled={cartItems.length === 0}
-              >
-                Siguiente
-              </button>
-              <button
-                  onClick={clearCart}
-                  className="clear-cart-button"
-                  style={{ marginLeft: '15px', marginTop: '15px' }}
-                  disabled={cartItems.length === 0}
-              >
-                Vaciar Carrito
-              </button>
->>>>>>> Stashed changes
-            </div>
-          </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default Cart;
