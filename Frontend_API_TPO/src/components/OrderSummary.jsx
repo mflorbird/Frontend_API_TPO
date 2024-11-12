b4295adebaa35fafed2994fbdc2c47e7bb70c56e
Diego B <dbari@seidoranalytics.com>
2024-11-11 23:49:58 -0300

    Update OrderSummary.jsx

--- Frontend_API_TPO/src/components/OrderSummary.jsx
+++ Frontend_API_TPO/src/components/OrderSummary.jsx
@@ -1,22 +1,6 @@
 import React from 'react';
 
 const OrderSummary = ({ subtotal, discountAmount, totalAmount }) => {
-<<<<<<< Updated upstream
-  return (
-    <div className="cart-summary">
-      <h3>Resumen de compra</h3>
-      <div className="summary-item">
-        <p className='mt-3'>Subtotal: ${subtotal}</p>
-      </div>
-      <div className="summary-item mt-2">
-        <p>Descuento: ${discountAmount}</p>
-      </div>
-      <div className="summary-item mt-2">
-        <p>Total: ${totalAmount}</p>
-      </div>
-    </div>
-  );
-=======
     return (
         <div className="cart-summary">
             <h3>Resumen de compra</h3>
@@ -24,14 +8,13 @@ const OrderSummary = ({ subtotal, discountAmount, totalAmount }) => {
                 <p>Subtotal: ${subtotal.toFixed(2)}</p>
             </div>
             <div className="summary-item">
-                <p>Descuento: %{(discountAmount * 100).toFixed(0)}</p>
+                <p>Descuento: %{(discountAmount * 100).toFixed(2)}</p>
             </div>
             <div className="summary-item">
                 <p>Total: ${totalAmount.toFixed(2)}</p>
             </div>
         </div>
     );
->>>>>>> Stashed changes
 };
 
 export default OrderSummary;
