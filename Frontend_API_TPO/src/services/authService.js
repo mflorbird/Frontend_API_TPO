689b6ec0e22f519fee3bdc7c282f146ff4a671f3
Diego B <dbari@seidoranalytics.com>
2024-11-23 17:43:36 -0300

    Update authService.js

--- Frontend_API_TPO/src/services/authService.js
+++ Frontend_API_TPO/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/users';
+const API_URL = 'http://localhost:8080/api/v1/authenticate/';
 
 export const loginUser = async (usuarioOEmail, contraseña) => {
   const params = {
@@ -9,20 +9,23 @@ export const loginUser = async (usuarioOEmail, contraseña) => {
   };
 
   try {
-    const response = await axios.get(API_URL, { params });
-    const user = response.data[0];
+    const response = await axios.post(API_URL + 'auth',  params );
+    const user = response.data;
 
 
-    
     if (user) {
-      const usersWithId1 = await axios.get(`${API_URL}?idUsuario=1`);
-      const userWithId1 = usersWithId1.data[0];
 
-      if (userWithId1 && userWithId1.id !== user.id) {
-        await axios.patch(`${API_URL}/${userWithId1.id}`, { idUsuario: '' });
-      }
+      // save user as JWT in local storage
+        localStorage.setItem('JWT', JSON.stringify(user));
 
-      await axios.patch(`${API_URL}/${user.id}`, { idUsuario: 1 });
+      // const usersWithId1 = await axios.get(`${API_URL}?idUsuario=1`);
+      // const userWithId1 = usersWithId1.data[0];
+      //
+      // if (userWithId1 && userWithId1.id !== user.id) {
+      //   await axios.patch(`${API_URL}/${userWithId1.id}`, { idUsuario: '' });
+      // }
+      //
+      // await axios.patch(`${API_URL}/${user.id}`, { idUsuario: 1 });
 
       return user;
     } else {
@@ -36,7 +39,8 @@ export const loginUser = async (usuarioOEmail, contraseña) => {
 
 export const registerUser = async (userData) => {
   try {
-    const response = await axios.post(API_URL, userData);
+    const response = await axios.post(API_URL + 'register', userData);
+    localStorage.setItem('JWT', JSON.stringify(response.data));
     return response.data;
   } catch (error) {
     throw new Error('Error al realizar la solicitud de registro');
